efa96d077ac06fe303922740d6e0cb639903488b
GopiRam <gopi.thungam.001@gmail.com>
2025-12-09 13:19:01 -0600

    Add game logic to game3.py

--- game3.py
+++ game3.py
@@ -37,11 +37,81 @@ class Player:
     def remove_one(self):
         return self.all_cards.pop(0)
 
-    def add_one(self,new_cards):
+    def add_cards(self,new_cards):
         
         if type(new_cards)==type([]):
-            self.all_cards.extend(new_card)
+            random.shuffle(new_cards)
+            self.all_cards.extend(new_cards)
         else:
             self.all_cards.append(new_cards)        
     def __str__(self):
         return f"Player {self.name} has {len(self.all_cards)} cards."
+
+player_one=Player("One")
+player_two=Player("Two")    
+
+new_deck=Deck()
+new_deck.shuffle()
+
+for x in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+
+game_on=True
+
+count=0
+while game_on:
+    count+=1
+    print(f"Round {count}")
+
+    if len(player_one.all_cards)==0:
+        print("Player One wins")
+        game_on=False
+        break
+    if len(player_two.all_cards)==0:
+        print("Player Two wins")
+        game_on=False
+        break   
+
+    player_one_cards=[]
+    player_one_cards.append(player_one.remove_one())
+
+    player_two_cards=[]
+    player_two_cards.append(player_two.remove_one())    
+
+    at_war = True
+
+
+    while at_war:
+        
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+            player_one.add_cards(player_one_cards)
+            player_one.add_cards(player_two_cards)
+            at_war = False
+
+        elif player_one_cards[-1].value < player_two_cards[-1].value:
+            player_two.add_cards(player_one_cards)
+            player_two.add_cards(player_two_cards)
+            at_war = False
+
+        else:
+           
+            print("war!")
+
+            if len(player_one.all_cards)<5:
+                print("player one unable to play game ")
+                print('Player Two wins')
+                game_on = False
+                break
+
+            elif len(player_two.all_cards ) <5:
+                print('Player two unable to play game   ')
+                print('Player One wins')
+                game_on = False
+                break
+            
+            else:
+
+                for i in range(5):
+                    player_one_cards.append(player_one.remove_one())
+                    player_two_cards.append(player_two.remove_one())
