d06e565d70331fe1f87164d6c48bfbc7e6644494
GopiRam <gopi.thungam.001@gmail.com>
2025-12-10 10:02:12 -0600

    Black Jack Game

--- game6.py
+++ game6.py
@@ -39,18 +39,18 @@ class Hand:
     def __init__(self):
         
         self.cards = []
-        
+        self.value = 0 
         self.aces=0
         
     def add_cards(self,card):
         
         self.cards.append(card)
         
-        self.value += values[Card.rank]
+        self.value += values[card.rank]
         
-        if Card.rank == 'Ace':
+        if card.rank == 'Ace':
             
-            self.aces = 1
+            self.aces += 1
             
     def adjust_for_aces(self):
         
@@ -114,17 +114,20 @@ def hit_or_stand(deck,hand):
         if x[0].lower() == 'h':
             
             hit(deck,hand)
-            
+            break
+        
         elif x[0].lower() == 's':
             
             print('Player Stands,Dealer is playing')
             
             playing= False
+            break
         else:
             print('Sorry I did not understand')
             
             continue
         
+        break
 
 def show_some(player,dealer):
     
@@ -169,6 +172,81 @@ def push(player,dealer):
     print("Tie It's a push")
 
 
+
+print('Welcome to Black Jack Game!')
+
+while True:
+    
+    playing = True
+    
+    
+    new_deck = Deck()
+    
+    new_deck.shuffle()
+    
+    
+    player_hand = Hand()  
+    player_hand.add_cards(new_deck.deal_one())
+    player_hand.add_cards(new_deck.deal_one())
+    
+    dealer_hand = Hand()
+    dealer_hand.add_cards(new_deck.deal_one())
+    dealer_hand.add_cards(new_deck.deal_one())
+    
+    chips = Chips()
+    
+    take_bet(chips)
+    
+    show_some(player_hand,dealer_hand)
+    
+    while playing:
+        
+            hit_or_stand(new_deck,player_hand)
+            
+            if not playing:
+                break
+            
+            show_some(player_hand,dealer_hand)
+            
+            if player_hand.value >21:
+                
+                player_busts(player_hand,dealer_hand,chips)
+                
+                break
+            
+    
+    if player_hand.value <= 21:
+        
+        while dealer_hand.value < 17:
+            hit(new_deck,dealer_hand)
+            
+        show_all(player_hand,dealer_hand)
+        
+        if dealer_hand.value >21 :
+
+                dealer_busts(player_hand,dealer_hand,chips)
+                
+        elif dealer_hand.value > player_hand.value:
+            dealer_win(player_hand,dealer_hand,chips)
+        elif dealer_hand.value < player_hand.value:
+            player_win(player_hand,dealer_hand,chips)
+
+        else:
+            
+            push(player_hand,dealer_hand)
+    
+    print(f"\nPlayer's total chips: {chips.total}")
+        
+    new_game = input('would you like to play again y or n') 
+    
+    if new_game[0].lower() == 'y':
+        playing = True
+        continue
+    else:
+        print('Thank you!')
+        playing= False
+        break           
+        
     
     
 
