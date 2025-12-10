3dd3a9c353f5ee68a8c1f5bcbac93c4eff099279
GopiRam <gopi.thungam.001@gmail.com>
2025-12-09 22:46:04 -0600

    Card Game

---
+++ game4.py
@@ -0,0 +1,169 @@
+import random
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 
+            'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
+
+
+class Card:
+    
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+        
+        
+    def __str__(self):
+        
+        return self.rank + ' of '+self.suit
+
+
+class Deck:
+    
+    def __init__(self):
+        
+        self.all_cards = []
+        
+        for suit in suits:
+            
+            for rank in ranks :
+                
+                self.all_cards.append(Card(suit,rank))
+                
+    def __str__(self):
+        
+        pass
+    
+    def shuffle(self):
+        
+        return random.shuffle(self.all_cards)
+
+    def deal_one(self):
+        
+        return self.all_cards.pop(0)
+    
+
+
+class Player():
+    
+    def __init__(self,name):
+        
+        self.name = name 
+        self.all_cards = []
+        
+    def add_cards(self,new_cards):
+        
+        if type(new_cards) == type([]):
+            
+            random.shuffle(new_cards)
+            
+            self.all_cards.extend(new_cards)
+        else:
+            
+            self.all_cards.append(new_cards)
+    
+    def remove_one(self):
+        
+        return self.all_cards.pop()
+    
+
+player_one = Player('One')
+
+player_two = Player('two')
+
+
+new_deck = Deck()
+
+new_deck.shuffle()
+
+
+for i in range(26):
+    
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+    
+Round_Number = 0
+
+game_on = True
+
+while game_on:
+    
+    Round_Number +=1
+    
+    print(f"\nRound {Round_Number}")
+    
+    if len(player_one.all_cards) ==0:
+        
+            print('Player One out of cards!')
+            print('Player Two Wins!') 
+            game_on = False
+            break
+            
+    if len(player_two.all_cards)== 0:
+        
+        print('Player Two out of cards!')
+        print('Player One Wins!') 
+        game_on = False
+        break
+    
+    player_one_cards = []
+    
+    player_one_cards.append(player_one.remove_one())
+    
+    player_two_cards = []
+    
+    player_two_cards.append(player_two.remove_one())
+    
+    print(f"Player One: {player_one_cards[0]}")
+    print(f"Player Two: {player_two_cards[0]}")
+    
+    at_war = True
+    
+    while at_war:
+        
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+            
+            player_one.add_cards(player_one_cards)
+            player_one.add_cards(player_two_cards)
+            
+            print("Player One wins the round")
+            
+            at_war = False
+            
+        elif player_one_cards[-1].value < player_two_cards[-1].value:
+            
+            player_two.add_cards(player_one_cards)
+            player_two.add_cards(player_two_cards)
+            
+            print("Player Two wins the round")
+            
+            at_war = False
+            
+        else:
+            
+            print("WAR!")
+            
+            if len(player_one.all_cards) < 5:
+                
+                print('Player One cannot continue')
+                print('Player Two Wins!')
+                
+                game_on = False
+                break
+            elif len(player_two.all_cards) <5:
+                
+                print('Player Two cannot continue')
+                print('Player One Wins!')
+                
+                game_on = False
+                break
+            
+            else:
+                
+                for i in  range(5):
+                    
+                    player_one_cards.append(player_one.remove_one())
+                    player_two_cards.append(player_two.remove_one())
+
+print(f"\nGame Over! Total rounds: {Round_Number}")
